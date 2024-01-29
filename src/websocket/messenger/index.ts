944454f4f3d3d375db9b03315d123a7096822e2d
Decagon <decagon@APPLEs-MacBook-Air.local>
2024-01-29 16:49:47 +0100

    update

--- src/websocket/messenger/index.ts
+++ src/websocket/messenger/index.ts
@@ -1,5 +1,3 @@
-
-
 export const customerServiceChatSocket = (io: any) => {
   io.on("connection", (socket: any) => {
     // to store agents active online
@@ -9,27 +7,30 @@ export const customerServiceChatSocket = (io: any) => {
     socket.on("agentLogin", (data: { agentId: string; username: string }) => {
       agents[socket.id] = data.agentId;
       const activeAgent = data.username;
-  
+
       io.emit("activeAgent", { activeAgent, status: "online" });
     });
 
     // to broadcast customers message to the agents active
-    socket.on("customerMessage", (data: { userId: string; message: string }) => {
-      const customer = data.userId;
-      const message = data.message;
-      const timestamp = new Date().toISOString();
+    socket.on(
+      "customerMessage",
+      (data: { userId: string; message: string }) => {
+        const customer = data.userId;
+        const message = data.message;
+        const timestamp = new Date().toISOString();
 
-      const incomingMessage = {
-        customer,
-        message,
-        timestamp,
-      };
+        const incomingMessage = {
+          customer,
+          message,
+          timestamp,
+        };
 
-      io.emit("messageAgent", incomingMessage);
-    });
+        io.emit("messageAgent", incomingMessage);
+      }
+    );
 
     // direct message between agent and customer
-    socket.on("directMessage", (data: {userId: string, message: string }) => {
+    socket.on("directMessage", (data: { userId: string; message: string }) => {
       const { userId, message } = data;
       const userSocketId = agents[userId];
 
@@ -45,10 +46,9 @@ export const customerServiceChatSocket = (io: any) => {
     socket.on("agentLogout", () => {
       const agentId = agents[socket.id];
       if (agentId) {
-
         delete agents[socket.id];
 
-        io.emit('activeAgent', { agentId, status: 'offline' });
+        io.emit("activeAgent", { agentId, status: "offline" });
       }
     });
   });
